f198de050076c86c5dd5c8119413a1279b99ca83
lydiateinfalt <60182389+lydiateinfalt@users.noreply.github.com>
2021-01-30 17:14:20 -0500

    Homework 2 update

--- Homework/Class-Ex-Lecture2.py
+++ Homework/Class-Ex-Lecture2.py
@@ -58,14 +58,14 @@ if die == 6:
     print(half_left + half_right)
 print("+--------+")
 print()
-print('#',75*"-")
+print('#', 75*"-")
 
 # =================================================================
 # Class_Ex2:
 # Answer  Ex1 by using functions.
 # Write function within function
 # ----------------------------------------------------------------
-print("Class_Ex2")
+print("Class_Ex2: Answer Ex1 by using functions. Write function within function.")
 from random import randrange
 def roll():
     return randrange(1,7)
@@ -120,81 +120,87 @@ def display_die(n):
     print("+--------+")
 
 def main():
+    print("This program rolls the die 3x to demonstrate randomness.")
     for i in range(0,3):
         display_die(roll())
 
 main()
 print()
-print('#',75*"-")
+print('#', 75*"-")
 # =================================================================
 # Class_Ex3: 
 # Randomly Permuting a List
 # ----------------------------------------------------------------
-print("Class_Ex3")
+print("Class_Ex3: Randomly permuting a List. ")
 def random_permute(a):
     import random
     new_list= a[:]
     random.shuffle(new_list)
-    print("New list: ", new_list)
+    return new_list
 
 
 original_list = [1, 8, 99, 17]
 print("Original list: ", original_list)
-random_permute(original_list)
+print("Randomly permuted list: ", random_permute(original_list))
 print()
-print('#',75*"-")
+print('#', 75*"-")
 
 # =================================================================
 # Class_Ex4:
 # Write a program to convert a tuple to a string.
 # ----------------------------------------------------------------
-"""
-t = tuple("lupins")
-print("Print tuple: ", t)
-print(''.join(t))
-print('#',75*"-")"""
+print("Class_Ex4: Write a program to convert a tuple to a string.")
+t = tuple("Monsieur Lupin")
+print("Tuple: ", t)
+print("String: ",''.join(t))
+print('#', 75*"-")
 
 # =================================================================
 # Class_Ex5:
 # Write a program to get the 3th element of a tuple.
 # ----------------------------------------------------------------
-"""
+print("# Class_Ex5:  Write a program to get the 3th element of a tuple.")
 t = tuple("lupins")
-print(t[2])
-print('#',75*"-")"""
+# Python counts start with 0 so the 3th element would be 2.
+print("Tuple: ", t)
+print("Tuple's 3th element: ", t[2])
+print('#', 75*"-")
 
 # =================================================================
 # Class_Ex6:
 # Write a program to check if an element exists in a tuple or not.
 # ----------------------------------------------------------------
-"""
-t = tuple("lupins")
-n = "1"
+print("Class_Ex6: Write a program to check if an element exists in a tuple or not.")
+t = tuple("apocalypse")
+# n = "a"
+n = 1
 if n in t:
     print("Yes, element", n, "is in tuple", t)
 else:
     print("No, element", n, "is not in tuple", t)
-print('#',75*"-")
-"""
+print('#', 75*"-")
+
 # =================================================================
 # Class_Ex7:
 # Write a program to check a list is empty or not.
 # ----------------------------------------------------------------
-"""
-mylist = ['a', 8.9, 99, [10,20]]
+print("Class_Ex7: Write a program to check a list is empty or not.")
+# mylist = ['a', 8.9, 99, [10,20]]
+# mylist = [1, 2, 'Amir', 19, -3, 'end']
+mylist = []
 
 if not mylist:
     print("List is empty")
 else:
-    print("List is not empty= ", mylist)
+    print("List is not empty = ", mylist)
 
-print('#',75*"-")
-"""
+print('#', 75*"-")
+
 # =================================================================
 # Class_Ex8:
 # Write a program to generate a 4*5*3 3D array that each element is O.
 # ----------------------------------------------------------------
-"""
+print("Class_Ex8: Write a program to generate 4*5*3 3D array that each element is 0.")
 my_3D_array = []
 x=4
 y=5
@@ -206,7 +212,5 @@ for i in range(x):
         for k in range(z):
             my_3D_array[i][j].append([0])
 
-print(my_3D_array)
-print('#',75*"-")
-
-"""
+print("The 4*5*3 3D array that each element is O", my_3D_array)
+print('#', 75*"-")
--- Homework/DATS_6103_LTeinfalt_Homework2.py
+++ Homework/DATS_6103_LTeinfalt_Homework2.py
@@ -17,19 +17,21 @@
 # i. Convert the following algorithm to a proper code and explain the process of the search.
 # ii. What are the pro and cons of this method?
 # ----------------------------------------------------------------
-
+print('#', 75 * "-")
+print("Homework_E.1:")
 import math
 
+
 def guess_calc(min, max):
-    guess_float = (min + max) / 2
+    guess_float = float((min + max) / 2)
     guess = int(math.floor(guess_float))
     return guess
+
 
 def search(array, n, target):
     min = 0
     max = n - 1
-    guess = (min + max) /2
-    guess = math.floor(guess)
+    guess = guess_calc(min, max)
     while guess in range(0, n):
         if array[guess] == target:
             return guess
@@ -37,48 +39,54 @@ def search(array, n, target):
             min = guess + 1
         else:
             max = guess - 1
-        guess = (min + max) /2
-        guess = math.floor(guess)
+        guess = guess_calc(min, max)
 
-"""def main():
+
+def main():
     # l is the array input
     # l = [99, 88, 77, 66, 55, 44, 22]
-    # l = [11, 22, 33, 44, 55]
+    l = [11, 22, 33, 44, 55]
     # l = [5, 22, 1, 33]
-    l =[99, 88, 77, 22]
+    # l =[99, 88, 77, 22]
     # n number of elements in th array
     n = len(l)
     # target is the number being searched for in the array
     target = 22
     # index in array l matching target
-    index = search(l, n, target)
-    print("target", target)
-    print("index", index)
+    index = int(search(l, n, target))
+    print("List: ", l)
+    print("Target: ", target)
+    print("Index: ", index)
     print("List[n] ", l[index])
 
-main()"""
-"""
-print("The algorithm is not correct as is currently stated in the pseudo code. Using the criteria array[guess] < target 
-to advance or subtract the array index is flawed. It assumes that the array of values is sorted in ascending order. If
-the array of values are sorted, then the algorithm would be a very efficient way to search for a value.
-")
-"""
+
+main()
+
+print(
+    "The algorithm is not correct as is currently stated in the pseudo code. Using the criteria array[guess] < target")
+print("to compare values in a list when the values not in order is flawed logic.")
+print("It assumes that the array is sorted in ascending order in order the algorithm to work.")
+print("If the array is sorted, it would be an efficient algorithm to search for a value in an array.")
+print('#', 75 * "-")
+
 # =================================================================
 # E.2:
 # Work on a script to count the number of characters or (frequency) in a string.
 # =================================================================
-"""
+print("Homework_E.2: Work on a script to count the number of characters or (frequency) in a string.")
+
+
 def freq_counter(search_string, pattern):
     x = len(search_string)
     index = 0
     count = 0
     while index < len(search_string):
-        #print(search_string[index])
+        # print(search_string[index])
         if pattern in search_string[index]:
             count = count + 1
         index += 1
     if count > 0:
-        print("|  " + pattern + "| "+ str(count) + " |")
+        print("|  " + pattern + "| " + str(count) + " |")
         # print("Character ", pattern, "appears ", count, "times." )
 
 
@@ -87,13 +95,17 @@ def main():
     for i in range(0, len(input_string)):
         freq_counter(input_string, input_string[i])
 
+
 main()
-"""
+
+print('#', 75 * "-")
 # =================================================================
 # E.3:
 # Write a function that takes a list of words and returns the length of the longest one.
 # =================================================================
-"""
+print("Homework_E.3: Write a function that takes a list of words and returns the length of the longest one.")
+
+
 def find_longest_word(l1):
     n = len(l1)
     temp_len = 0
@@ -104,70 +116,86 @@ def find_longest_word(l1):
             max_len = temp_len
     return max_len
 
+
 def main():
     l = ["Unless", "you're", "Python", "unanimous"]
     longest = find_longest_word(l)
     print("Longest word length", longest)
 
+
 main()
-"""
+print('#', 75 * "-")
+
 # =================================================================
 # E.4:
 # Make up your own list and work on a program to get the smallest number from the list.
 # =================================================================
-"""
+print("Homework_E.4: Make up your own list and work on a program to get the smallest number from the list.")
+
 
 def main():
-    l_num = [99,1,45,3,8, 50, -1, -100]
+    l_num = [99, 1, 45, 3, 8, 50, -1, -100]
     l_num.sort()
     print("List", l_num)
     print("Smallest in list", l_num[0])
 
+
 main()
-"""
+
+print('#', 75 * "-")
 # =================================================================
 # E.5:
 # Work on a function that takes two lists and returns same (or True) if they have at least one
 # common element.
 # =================================================================
-"""
+print("Homework_E.5: Work on a function that takes two lists and returns same (or True) if they have at least one.")
+
+
 def element_in_common(l1, l2):
     m = len(l1)
     n = len(l2)
-    for i in range (0, m):
+    for i in range(0, m):
         if l1[i] in l2:
             return True
         test = False
     return test
+
 
 def main():
     test_list1 = [1, 4, 77, -3, "Amir"]
     test_list2 = [2, 5, 66, "Amir", 0]
     if element_in_common(test_list1, test_list2):
         print("Yes, the two lists have at least one element in common.")
+
+
 main()
-"""
+print('#', 75 * "-")
+
 # =================================================================
 # E.6: Work on a script to merge or join two dictionaries
 # =================================================================
-"""
+print("Homework_E.6:  Work on a script to merge or join two dictionaries")
+
+
 def merge(d1, d2):
-    return(d2.update(d1))
+    return (d2.update(d1))
 
 
 my_dict1 = {'a': 1, 'b': 2, 'c': "3"}
 my_dict2 = {'d': 4, 'e': 5, 'f': "6"}
 print(merge(my_dict1, my_dict2))
 print(my_dict2)
-"""
+
+print('#', 75 * "-")
 # =================================================================
 # E.7: Work on a script or a program to map two lists into a dictionary.
 # =================================================================
+print("Homework_E.7: Work on a script or a program to map two lists into a dictionary.")
 keys = ["un", "deux", "trois", "quatre", "cinq"]
 values = ["one", "two", "three", "four", "five"]
-new_dict=dict(zip(keys,values))
+new_dict = dict(zip(keys, values))
 print(new_dict)
-
+print('#', 75 * "-")
 # =================================================================
 # E.8: Answer all the class exercise questions and submit it (Check the instructions).
 # =================================================================
