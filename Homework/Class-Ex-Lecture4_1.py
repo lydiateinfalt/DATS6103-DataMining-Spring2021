d242e8a6926db964af399617d65b2ff987fac7bf
lydiateinfalt <60182389+lydiateinfalt@users.noreply.github.com>
2021-02-23 15:23:09 -0500

    Lecture and HW 4 update

--- Homework/Class-Ex-Lecture4_1.py
+++ Homework/Class-Ex-Lecture4_1.py
@@ -136,12 +136,8 @@ print('#',50*"-")
 # E.9:
 # Answer all the class exercise questions (Lecture 4 - All) and submit it (Check the instructions).
 
-# =================================================================
-# Class_Ex1:
-# Write a NumPy code to test if none of the elements of a given
-# array is zero.
-# ----------------------------------------------------------------
 
+# Solution Class_Ex1: Write Numpy code to test if none of the elements of an array is zero
 # Reference: https://www.w3resource.com/python-exercises/numpy/basic/index.php
 import numpy as np
 
@@ -163,14 +159,8 @@ else:
 
 print('#',50*"-")
 
-
-
-# =================================================================
-# Class_Ex2:
-# Write a NumPy code to test if none of the elements of a given
-# array is non-zero.
-# ----------------------------------------------------------------
-#Solution Class_Ex2: none of the elements of a given array is non-zero
+# Solution Class_Ex2: none of the elements of a given array is non-zero
+# Reference: https://www.w3resource.com/python-exercises/numpy/basic/index.php
 x = [0,0,0,0,0]
 print("Original array", x)
 print(not np.any(x))
@@ -185,7 +175,8 @@ print('#',50*"-")
 # Write a NumPy code to test if two arrays are element-wise equal
 # within a tolerance.
 # ----------------------------------------------------------------
-#Solution Class_Ex3: Write a NumPy code to test if two arrays are element-wise equal within a tolerance
+# Solution Class_Ex3: Write a NumPy code to test if two arrays are element-wise equal within a tolerance
+# Reference: https://www.w3resource.com/python-exercises/numpy/basic/index.php
 ar1 = [0.6, 0.1, -0.0001]
 ar2 = [0.4, 0.2, -0.1]
 print("Original arrays", ar1, "", ar2)
@@ -206,11 +197,7 @@ a = np.array([1, 8, 130, 10990005])
 print("Input array", a)
 print("Memory size of array (bytes)", a.size * a.itemsize)
 print('#',50*"-")
-# =================================================================
-# Class_Ex5:
-# Write a NumPy code to create a array with values ​​ranging from
-# 10 to 20 and print all values ​​except the first and last.
-# ----------------------------------------------------------------
+
 #Solution Class_Ex5
 x = np.random.randint(10, 20, size = 10)
 print("Original array:", x)
@@ -218,55 +205,39 @@ print("Modified array without the first or last element", x[1:len(x)-1])
 
 
 print('#',50*"-")
-# =================================================================
-# Class_Ex6:
-# Write a NumPy code to reverse (flip) an array (first element becomes last).
-# ----------------------------------------------------------------
+
 # Solution Class_Ex6: Numpy code to reverse (flip) an array
 
 y = np.random.randint(0,100, size = 20)
 print("Original array", y)
 y = y[::-1]
 print("Flipped (reversed) array", y)
-
-
-
-
 print('#',50*"-")
 
 # Solution Class_Ex 7: Write a NumPy code to create a matrix with 1 on the border and 0 inside.
+# Reference: https://www.w3resource.com/python-exercises/numpy/basic/index.php
 xray = np.ones((7,7))
 print("Original matrix:", xray)
 xray[1:-1,1:-1] = 0
 print("Modified:", xray)
-
-
-
-
-
 print('#',50*"-")
 
 
 # Solution Class_Ex8: Write a NumPy code to add a border (filled with 0's) around a 3x3 matrix of one.
+# Reference: https://www.w3resource.com/python-exercises/numpy/basic/index.php
 ray3 = np.ones([3,3])
 print("Original matrix:", ray3)
 ray3=np.pad(ray3, pad_width=1, mode='constant', constant_values= 0)
 print("Modified:\n", ray3)
-
-
-
-
 print('#',50*"-")
+
 # Solution Class_Ex9: Write a NumPy code to append values to the end of an array.
 ex9_array = np.array([99, 88, 77])
 np.append(arr=ex9_array, values=(66, 55))
-
-
-
-
 print('#',50*"-")
 
-# Solution Class_Ex10: NumPy code to find the set difference of two arrays.The set difference will return the sorted, unique values in array1 that are not in array2.
+# Solution Class_Ex10: NumPy code to find the set difference of two arrays.
+# The set difference will return the sorted, unique values in array1 that are not in array2.
 
 array1 = np.array([21, 16, 6, 26, 1, 13, 21, 1, 15, 28])
 array2 = np.array([2, 4, 22, 11, 3, 7, 24, 12, 13, 20])
@@ -276,23 +247,16 @@ array3 = np.unique(array3)
 print("Array1:", array1)
 print("Array2:", array2)
 print("Unique values in array 1 that are not in array2\n", array3)
-
-
-
-
 print('#',50*"-")
-# Solution Class_Ex11: Write a NumPy code to construct an array by repeating. Sample array: [1, 2, 3, 4, 5]
 
+# Solution Class_Ex11: Write a NumPy code to construct an array by repeating. Sample array: [1, 2, 3, 4, 5]
 n = np.array([1, 2, 3, 4, 5])
 new_array = np.tile(n,5)
 print(new_array)
-
-
 print('#',50*"-")
+
 # Solution Class_Ex12: Write a NumPy code to get the values and indices of the elements
 # that are bigger than 6 in a given array.
-import numpy as np
-
 x = np.array([1,99, 0, 17, -8, 11, 66])
 print("Values bigger than 6 =", x[x>6])
 print("Their indices are ", np.where(x > 6))
@@ -300,7 +264,6 @@ print('#',50*"-")
 
 
 # Solution Class_Ex13: Write Numpy code to get the 4th element of a 2 dimensional array
-
 x = np.array([[2,5,7],[8,9,10]])
 print("Original array \n")
 print(x)
@@ -309,75 +272,72 @@ print('#',50*"-")
 
 
 # Solution Class_Ex14: Write a NumPy code to get the floor, ceiling and truncated values of the elements of an numpy array.
-
 x = [1.52421, 3.1123, - 1.053, 0.04354507, 2.19]
 print(x)
-
-
 print("Floor values:",np.floor(x))
 print("Ceiling values:",np.ceil(x))
 print("Truncated values:", np.trunc(x))
 print('#',50*"-")
-# =================================================================
-# Class_Ex15:
-# Write a NumPy code to compute the factor of a given array by
-# Singular Value Decomposition.
-# ----------------------------------------------------------------
+
+# Solution for Class_Ex15: Write a NumPy code to compute the factor of a given array by Singular Value Decomposition.
+# Reference: https://www.w3resource.com/python-exercises/numpy/linear-algebra/numpy-linear-algebra-exercise-18.php
+
+x = np.array([[3, 4, 1, 4], [4, 2, 3, 2], [3, 0, 4, 4], [3, 2, 4, 4]])
+print(x)
+U, s, V = np.linalg.svd(x, full_matrices=False)
+q, r = np.linalg.qr(x)
+print("Factor of a given array  by Singular Value Decomposition:")
+print("U=\n", U, "\ns=\n", s, "\nV=\n", V)
+print('#',50*"-")
+
+# Solution Class_Ex16: Write a NumPy code to compute the eigenvalues and right eigenvectors of a given square array.
+# Reference: https://www.w3resource.com/python-exercises/numpy/linear-algebra/numpy-linear-algebra-exercise-18.php
+x = np.array([[1, 3, 5],
+              [2, 1, 0],
+              [2, 4, 6]])
+y,z = np.linalg.eig(x)
+print("Eigenvalues", y)
+print("Right eigenvalues\n", z)
+print('#',50*"-")
 
 
+# Solution CLass_Ex17: Write a NumPy code to get the dates of yesterday, today and tomorrow.
+# https://www.w3resource.com/python-exercises/numpy/python-numpy-datetime-exercise-2.php
+today = np.datetime64("today")
+yesterday = today - np.timedelta64(1, 'D')
+tomorrow = today + np.timedelta64(1, 'D')
+print(yesterday)
+print(today)
+print(tomorrow)
+print('#',50*"-")
 
-
+# Solution Class_Ex18: Write a NumPy code to find the first Monday in June 2021.
+# https://www.w3resource.com/python-exercises/numpy/python-numpy-datetime-exercise-5.php
+print("First Monday in June 2021:")
+print(np.busday_offset('2021-06', 0, roll='forward', weekmask='Mon'))
+print('#',50*"-")
 
 print('#',50*"-")
-# =================================================================
-# Class_Ex16:
-# ----------------------------------------------------------------
-# Write a NumPy code to compute the eigenvalues and right eigenvectors
-# of a given square array.
-# ----------------------------------------------------------------
-
-
-
-print('#',50*"-")
-# =================================================================
-# Class_Ex17:
-# Write a NumPy code to get the dates of yesterday, today and tomorrow.
-# ----------------------------------------------------------------
-
-
-
-
-print('#',50*"-")
-# =================================================================
-# Class_Ex18:
-# Write a NumPy code to find the first Monday in June 2021.
-# ----------------------------------------------------------------
-
-
-
-print('#',50*"-")
-# =================================================================
-# Class_Ex19:
+# Solution Class_Ex19:
 # Write a NumPy code to find the roots of the following polynomials.
 # a) x2 − 3x + 8.
 # b) x4 − x3 + -x2 + 1x – 2
-# ----------------------------------------------------------------
+# https://www.w3resource.com/python-exercises/numpy/python-numpy-math-exercise-16.php
 
-
-
-
+print(np.roots([1,-3, 8]))
+print(np.roots([1, -1, -1, 1, 2]))
 print('#',50*"-")
-# =================================================================
-# Class_Ex20:
-# Write a NumPy program to calculate mean across dimension, of matrix.
-# ----------------------------------------------------------------
 
 
+# Solution Class_Ex20: Write a NumPy program to calculate mean across dimension, of matrix.
+x = np.arange(12).reshape((3, 4))
+print(x)
 
-
-
-
-
+print(x.mean())
+# print(x.mean(0))
+# print(x.mean(1))
+print(x.mean(axis=0))
+print(x.mean(axis=1))
 print('#',50*"-")
 
 
